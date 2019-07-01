677d2d816cc2d074ca8d2a9fde362af7abb584e5
Fareeha10 <42777932+Fareeha10@users.noreply.github.com>
2019-07-01 18:48:46 +0500

    Add files via upload

---
+++ EvenOdd.py
@@ -0,0 +1,10 @@
+def even(x):        #initialize function 
+    if x%2==0:
+        p=print("value is even")    
+    else:
+        p=print("value is odd")
+    return p       
+
+
+a=int(input("Enter value:"))    #calling function
+print(a)
